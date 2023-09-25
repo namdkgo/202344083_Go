2d0b97c838e35f00cf9ebd91af4158da7b192cd6
namdkgo <namdkgo@gmail.com>
2023-09-25 11:29:00 +0900

    week05 v0.7

--- Week05/pass_fail.go
+++ Week05/pass_fail.go
@@ -3,13 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, _ := reader.ReadString('\n')
+	//inputScore, _ := reader.ReadString('\n') 에러 해결법 1
+	inputScore, err := reader.ReadString('\n')
+	log.Fatal(err) // 에러 해결법 2
 	fmt.Println(inputScore)
-
 }
