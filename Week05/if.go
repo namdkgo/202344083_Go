36ea924a2599d81a1123e05a8a6a39e92b6d50d5
namdkgo <namdkgo@gmail.com>
2023-09-25 11:43:40 +0900

    week05 v0.9

--- Week05/if.go
+++ Week05/if.go
@@ -8,11 +8,19 @@ import (
 )
 
 func main() {
-	fmt.Print("Enter a grade: ")
+	fmt.Print("Enter a score: ")
 	reader := bufio.NewReader(os.Stdin)
 	inputScore, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if inputScore >= 90 { //invalid operation: inputScore >= 90 (mismatched types string and untyped int)
+		grade := "A"
+	} else {
+		grade := "under A grade"
+	}
+
+	fmt.Println(grade)
 	fmt.Println(inputScore)
 }
