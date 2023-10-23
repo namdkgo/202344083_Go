ec90ab3e80cc587f5040f55fa79df970b97cb3a0
namdkgo <namdkgo@gmail.com>
2023-10-23 12:12:22 +0900

    포인터 이용한 swap 함수 구현

--- Week09/pointer2.go
+++ Week09/pointer2.go
@@ -1,19 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func double(number *int) {
-	*number *= 2
+func swap(num1 *int, num2 *int) {
+	temp := *num1
+	*num1 = *num2
+	*num2 = temp
 }
 
 func main() {
-	var amount int = 5
-	double(&amount)
-	fmt.Println(amount)
+	a := 10
+	b := 20
+	var c *int = &a //integer 타입 변수는 integer 타입 포인터 변수에만 담을 수 있음.
+	fmt.Printf("%T\n", c)
+	d := &b
+
+	fmt.Println(a, b)
+	swap(&a, &b)
+	fmt.Println(a, b)
+	swap(c, d)
+	fmt.Println(a, b)
 }
-
-
-& *
-a
