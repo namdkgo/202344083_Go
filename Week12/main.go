482ca25c2a39b9b10dae65f9ad7f0002ffd6e185
namdkgo <namdkgo@gmail.com>
2023-11-13 11:39:21 +0900

    aa

--- Week12/main.go
+++ Week12/main.go
@@ -1,29 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 func main() {
-	s := []int{0, 0, 0, 0, 0} //slice 리터럴
-	s[4] = 99
-	s[2] = 11
-	for i := 0; i < len(s); i++ {
-		fmt.Println(s[i])
-	}
-	fmt.Println("  __  ")
-	copyS := s[1:5]
-	for i := 0; i < len(copyS); i++ {
-		fmt.Println(copyS[i])
-	}
+	a := []string{"a", "b", "c", "d"}
+	as := a[0:2]
+	as[1] = "z"
+	fmt.Println(as)
 
-	fmt.Println(reflect.TypeOf(copyS))
-
-	test := [3]string{"inha", "go", "student"}
-	testS := test[:2]
-	testS2 := test[1:]
-	test[1] = "1"
-	fmt.Println(testS[1])
-	fmt.Println(testS2[0])
+	b := [4]int{4, 3, 2, 1}
+	bs := b[1:3]
+	fmt.Println(bs)
 }
