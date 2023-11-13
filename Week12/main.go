81f0a8795610296121135b8a676b08525f43946f
namdkgo <namdkgo@gmail.com>
2023-11-13 11:34:26 +0900

    zz

--- Week12/main.go
+++ Week12/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	s := []int{0, 0, 0, 0, 0} //slice 리터럴
@@ -9,9 +12,18 @@ func main() {
 	for i := 0; i < len(s); i++ {
 		fmt.Println(s[i])
 	}
-
-	copyS := s[1:4]
+	fmt.Println("  __  ")
+	copyS := s[1:5]
 	for i := 0; i < len(copyS); i++ {
 		fmt.Println(copyS[i])
 	}
+
+	fmt.Println(reflect.TypeOf(copyS))
+
+	test := [3]string{"inha", "go", "student"}
+	testS := test[:2]
+	testS2 := test[1:]
+	test[1] = "1"
+	fmt.Println(testS[1])
+	fmt.Println(testS2[0])
 }
