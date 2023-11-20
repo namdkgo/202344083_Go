28c31893741ef6597c36572885a691d3abe23a9d
namdk <namdkgo@gmail.com>
2023-11-20 10:48:56 +0900

    append

--- Week13/main.go
+++ Week13/main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	var a []string
-	a = make([]string, 4, 5)
-	fmt.Printf("%#v\n", a)
+	var b []bool
+	//a = make([]string, 4, 5)
+	a = append(a, "abc")
+	b = append(b, true)
+	fmt.Printf("%#v %#v\n", a, b)
 	fmt.Println(a, len(a), cap(a))
+	fmt.Println(b, len(b), cap(b))
 }
